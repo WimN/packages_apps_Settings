3fd85ea3921c8882cd193949754c174d0358cece
John Brewer <gzroms@hotmail.com>
2015-01-13 21:35:05 -0500

    Change Density

--- src/com/android/settings/crdroid/AnimationSettings.java
+++ src/com/android/settings/crdroid/AnimationSettings.java
@@ -15,14 +15,24 @@
  */
 package com.android.settings.crdroid;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.app.IActivityManager;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.text.Editable;
 import android.widget.Toast;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
@@ -31,12 +41,18 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "AnimationSettings";
+	
+	private static final int DIALOG_DENSITY = 101;
 
     private static final String KEY_TOAST_ANIMATION = "toast_animation";
+	private static final String KEY_LCD_DENSITY = "lcd_density";
 
     private Context mContext;
 
+	private ListPreference mLcdDensityPreference;
     private ListPreference mToastAnimation;
+	
+	protected Context mContext;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -46,6 +62,11 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
 
         mContext = getActivity().getApplicationContext();
+		
+        PreferenceScreen prefSet = getPreferenceScreen();
+ 
+        mContext = getActivity().getApplicationContext();
+        int newDensityValue;
 
         // Toast Animations
         mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
@@ -56,6 +77,21 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
         mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
         mToastAnimation.setOnPreferenceChangeListener(this);
 
+        // LCD density
+        mLcdDensityPreference = (ListPreference) prefSet.findPreference(KEY_LCD_DENSITY);
+        int defaultDensity = DisplayMetrics.DENSITY_DEVICE;
+        String[] densityEntries = new String[9];
+        for (int idx = 0; idx < 8; ++idx) {
+            int pct = (75 + idx*5);
+            densityEntries[idx] = Integer.toString(defaultDensity * pct / 100);
+        }
+		densityEntries[8] = getString(R.string.custom_density);
+        int currentDensity = DisplayMetrics.DENSITY_CURRENT;
+        mLcdDensityPreference.setEntries(densityEntries);
+        mLcdDensityPreference.setEntryValues(densityEntries);
+        mLcdDensityPreference.setValue(String.valueOf(currentDensity));
+        mLcdDensityPreference.setOnPreferenceChangeListener(this);
+        updateLcdDensityPreferenceDescription(currentDensity);		
     }
 
     @Override
@@ -71,6 +107,94 @@ public class AnimationSettings extends SettingsPreferenceFragment implements
             Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
             return true;
         }
+        if (KEY_LCD_DENSITY.equals(key)) {
+            String strValue = (String) objValue;
+            if (strValue.equals(getResources().getString(R.string.custom_density))) {
+                showDialog(DIALOG_DENSITY);
+            } else {
+                int value = Integer.parseInt((String) objValue);
+                writeLcdDensityPreference(value);
+                updateLcdDensityPreferenceDescription(value);
+            }
+        }		
         return false;
     }
+
+    private void updateLcdDensityPreferenceDescription(int currentDensity) {
+        ListPreference preference = mLcdDensityPreference;
+        String summary;
+        if (currentDensity < 10 || currentDensity >= 1000) {
+            // Unsupported value
+            summary = "";
+        }
+        else {
+            summary = Integer.toString(currentDensity) + " DPI";
+        }
+        preference.setSummary(summary);
+    }
+
+    public void writeLcdDensityPreference(int value) {
+        try {
+            SystemProperties.set("persist.sys.lcd_density", Integer.toString(value));
+        }
+        catch (Exception e) {
+            Log.w(TAG, "Unable to save LCD density");
+        }
+        try {
+            final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+            if (am != null) {
+                am.restart();
+            }
+        }
+        catch (RemoteException e) {
+            Log.e(TAG, "Failed to restart");
+        }
+    }
+
+    public Dialog onCreateDialog(int dialogId) {
+        LayoutInflater factory = LayoutInflater.from(mContext);
+
+        switch (dialogId) {
+            case DIALOG_DENSITY:
+                final View textEntryView = factory.inflate(
+                        R.layout.alert_dialog_text_entry, null);
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(R.string.custom_density_dialog_title)
+                        .setMessage(getResources().getString(R.string.custom_density_dialog_summary))
+                        .setView(textEntryView)
+                        .setPositiveButton(getResources().getString(R.string.set_custom_density_set), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                EditText dpi = (EditText) textEntryView.findViewById(R.id.dpi_edit);
+                                Editable text = dpi.getText();
+                                Log.i(TAG, text.toString());
+                                String editText = dpi.getText().toString();
+
+                                try {
+                                    SystemProperties.set("persist.sys.lcd_density", editText);
+                                }
+                                catch (Exception e) {
+                                    Log.w(TAG, "Unable to save LCD density");
+                                }
+                                try {
+                                    final IActivityManager am = ActivityManagerNative.asInterface(ServiceManager.checkService("activity"));
+                                    if (am != null) {
+                                        am.restart();
+                                    }
+                                }
+                                catch (RemoteException e) {
+                                    Log.e(TAG, "Failed to restart");
+                                }
+                            }
+
+                        })
+                        .setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                                dialog.dismiss();
+                            }
+                        }).create();
+        }
+        return null;
+    }
+	
 }
