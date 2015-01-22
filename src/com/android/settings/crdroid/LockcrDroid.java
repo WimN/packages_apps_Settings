0dcd9bc80d8789d4d7243d671187dad6cfe3ef3d
John Brewer <gzroms@hotmail.com>
2015-01-22 17:38:05 -0500

    Revert "Revert "Settings: LS weather disabler (2/2)""
    This reverts commit 8190b46d9a7bde16c4df708c40bfb4758c862703.

--- src/com/android/settings/crdroid/LockcrDroid.java
+++ src/com/android/settings/crdroid/LockcrDroid.java
@@ -47,10 +47,12 @@ public class LockcrDroid extends SettingsPreferenceFragment
 
     private static final String KEY_LOCKSCREEN_CAMERA_WIDGET_HIDE = "camera_widget_hide";
     private static final String KEY_LOCKSCREEN_DIALER_WIDGET_HIDE = "dialer_widget_hide";
-
+	private static final String KEY_LOCKSCREEN_WEATHER = "lockscreen_weather";
+	
     private PreferenceScreen mLockScreen;
     private SwitchPreference mCameraWidgetHide;
     private SwitchPreference mDialerWidgetHide;
+	private SwitchPreference mLockscreenWeather;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -76,6 +78,12 @@ public class LockcrDroid extends SettingsPreferenceFragment
         if (mCameraDisabled){
             mLockScreen.removePreference(mCameraWidgetHide);
         }
+		
+        // Lockscreen weather
+        mLockscreenWeather = (SwitchPreference) findPreference(KEY_LOCKSCREEN_WEATHER);
+        mLockscreenWeather.setChecked(Settings.System.getIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER, 1, UserHandle.USER_CURRENT) == 1);
+        mLockscreenWeather.setOnPreferenceChangeListener(this);
 
         // Dialer widget hide
         mDialerWidgetHide = (SwitchPreference) prefSet.findPreference(KEY_LOCKSCREEN_DIALER_WIDGET_HIDE);
@@ -117,5 +125,10 @@ public class LockcrDroid extends SettingsPreferenceFragment
             Helpers.restartSystem();
         }
         return true;
+        } else if (preference == mLockscreenWeather) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.LOCKSCREEN_WEATHER, value ? 1 : 0, UserHandle.USER_CURRENT);
+            Helpers.restartSystem();		
     }
 }
